7591c3b2fd05e0309699b219e96c13710dbce492
Nikita <nicitaacom@gmail.com>
2024-01-15 19:52:05 +0100

    bump react email to 0.0.11
    It didn't help

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 "use client"
-import { render } from "@react-email/render"
+import { renderAsync } from "@react-email/render"
 import CheckEmail from "./emails/CheckEmail"
 import { formatDeliveryDate } from "./utils/formatDeliveryDate"
 
 export default function Home() {
   const deliveryDate = formatDeliveryDate()
 
-  const emailMessageString = render(<CheckEmail deliveryDate={deliveryDate} />, {
+  const emailMessageString = renderAsync(<CheckEmail deliveryDate={deliveryDate} />, {
     pretty: true,
   })
 
