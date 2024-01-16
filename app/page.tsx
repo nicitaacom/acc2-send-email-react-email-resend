a678734d5beb38f33d7dbe69a6a5b65efcb303ba
Nikita <nicitaacom@gmail.com>
2024-01-16 07:16:11 +0100

    tailwind updated - issue persists

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,23 @@
 import { renderAsync } from "@react-email/render"
 import CheckEmail from "./emails/CheckEmail"
 import { formatDeliveryDate } from "./utils/formatDeliveryDate"
+import { useEffect, useState } from "react"
 
 export default function Home() {
   const deliveryDate = formatDeliveryDate()
+  const [emailMessageString, setEmailMessageString] = useState("")
 
-  const emailMessageString = renderAsync(<CheckEmail deliveryDate={deliveryDate} />, {
-    pretty: true,
-  })
+  useEffect(() => {
+    async function renderEmail() {
+      const emailMessageString = await renderAsync(<CheckEmail deliveryDate={deliveryDate} />, {
+        pretty: true,
+      })
+      setEmailMessageString(emailMessageString)
+    }
+    renderEmail()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  console.log(18, "emailMessageString - ", emailMessageString)
 
-  return <div>hi</div>
+  return <div>{emailMessageString}</div>
 }
